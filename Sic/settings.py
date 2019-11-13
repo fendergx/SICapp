b99ad2e26f28dd49f1ef2cf584dfefd36b28b7ae
fendergx <fendergx@gmail.com>
2019-11-12 22:58:05 -0600

    Update settings.py
    allowed hosts

--- Sic/settings.py
+++ Sic/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '-@+aiuqm#a5ijq(mp=6xqi6f_8er69r(c+g!9x7w*5cq65nil2'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
 
 # Application definition
